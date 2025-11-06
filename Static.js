4949f46dbf9ffc5a7878081e358d626651ebd758
guptaautkarsh72dot <guptaautkarsh21@gmail.com>
2025-11-06 14:09:39 +0530

    Update Static.js

--- Static.js
+++ Static.js
@@ -1,4 +1,4 @@
 document.getElementById("clickMe").addEventListener("click", function() {
   const msg = document.getElementById("message");
   msg.textContent = "You clicked the button! 🚀";
-});
+}) ;
